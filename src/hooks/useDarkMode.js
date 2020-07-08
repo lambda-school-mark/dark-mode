d05a8a9990fdc1bdb8f9ac31f0ef0cc8afd18b48
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-07-08 18:45:00 -0400

    things happened and worked

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,2 +1,16 @@
-import { useLocalStorage } from "../hooks/useLocalStorage";
-export const useDarkMode = () => {};
+import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
+export const useDarkMode = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+  const toggleMode = (e) => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+  useEffect(() => {
+    const body = document.querySelector(".body");
+    darkMode
+      ? body.classList.add("dark-mode")
+      : body.classList.remove("dark-mode");
+  }, [darkMode]);
+  return [darkMode, toggleMode];
+};
